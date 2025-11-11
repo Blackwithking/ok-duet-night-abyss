69329a302ee8ed95d81314e78be1806c7e470a71
RU <110257560+BnanZ0@users.noreply.github.com>
2025-11-11 13:18:24 +0800

    停用AutoHDR检查

--- src/config.py
+++ src/config.py
@@ -85,7 +85,7 @@ config = {
         'hwnd_class': 'UnrealWindow', #增加重名检查准确度
         'interaction': 'PostMessage', #支持大多数PC游戏后台点击
         'capture_method': ['WGC', 'BitBlt_RenderFull'],  # Windows版本支持的话, 优先使用WGC, 否则使用BitBlt_Full
-        'check_hdr': True, #当用户开启AutoHDR时候提示用户, 但不禁止使用
+        'check_hdr': False, #当用户开启AutoHDR时候提示用户, 但不禁止使用
         'force_no_hdr': False, #True=当用户开启AutoHDR时候禁止使用
     },
     'start_timeout': 120,  # default 60
